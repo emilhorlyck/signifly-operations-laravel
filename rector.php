595ae4d56c2e43cc301b443ffaceb78b9bb85cf4
Emil Hørlyck <eh@signifly.com>
2025-03-01 06:59:38 +0100

    chore: run rector

--- rector.php
+++ rector.php
@@ -7,12 +7,12 @@ use Rector\Config\RectorConfig;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        // __DIR__ . '/bootstrap',
-        // __DIR__ . '/config',
-        // __DIR__ . '/public',
-        // __DIR__ . '/resources',
-        // __DIR__ . '/routes',
-        // __DIR__ . '/tests',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
     ])
     // uncomment to reach your current PHP version
     // ->withPhpSets()
