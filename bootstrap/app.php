d2603356da2412e676bd88a19cd3c6d82cae9077
Emil Hørlyck <eh@signifly.com>
2025-03-02 12:20:59 +0100

    chore: base config of webhooks

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'webhooks',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
